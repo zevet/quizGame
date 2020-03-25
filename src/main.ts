96881a00ba76da6c083c363a54b5d6a106aedaca
Carlos E <carloseduardo057@gmail.com>
2020-03-25 13:17:16 -0300

    saving changes

--- src/main.ts
+++ src/main.ts
@@ -15,15 +15,6 @@ engine.setAntialiasing(false);
 engine.add('game', new Game(engine));
 engine.goToScene('game');
 
-// Game events to handle
-engine.on('hidden', () => {
-    console.log('pause');
-    engine.stop();
-});
-engine.on('visible', () => {
-    console.log('start');
-    engine.start();
-});
 
 engine.input.keyboard.on('press', (evt: ex.Input.KeyEvent) => {
     if (evt.key === ex.Input.Keys.D) {
