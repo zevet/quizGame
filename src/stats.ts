def663d75271ed82a65038caa6ac3378d628e136
Carlos E <carloseduardo057@gmail.com>
2020-03-27 12:35:17 -0300

    excluido arquivos nao utilizados

--- src/stats.ts
+++ src/stats.ts
@@ -2,12 +2,10 @@ import Config from "./config";
 
 class Stats {
 	public bullets: number = 10;
-    public hp: number = Config.totalHp;
     public gameOver: boolean = false;
     public canShot = true;
     public reset() {
 		this.bullets = 10;
-        this.hp = Config.totalHp;
         this.gameOver = false;
     }
 }
